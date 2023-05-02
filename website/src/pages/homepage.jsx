09663ac77f0f4e6b9954cf70aa2074ded93838fe
Sunny <derpynoobmaster@gmail.com>
2023-05-02 17:28:39 +1000

    formatting + content

--- website/src/pages/homepage.jsx
+++ website/src/pages/homepage.jsx
@@ -3,8 +3,34 @@ import image from "../images/profile.jpg";
 const HomePage = () => {
     return (
         <>
-            <div style={{backgroundColor: "#081b29", height: "100vh"}}>home
-                <img src={image} style={{width: "30%"}}></img>
+            <div style={{ backgroundColor: "#081b29", height: "95vh" }}>
+                <div style={{ display: "flex", marginLeft: "2%" }}>
+                    <div
+                        style={{
+                            color: "white",
+                            margin: "9%",
+                            marginLeft: "9%",
+                            width: "33%",
+                        }}
+                    >
+                        <h1 style={{ fontSize: "56px" }}>Hi, I'm Sunny</h1>
+                        <h2 style={{ color: "#00abf0" }}>Software Engineer</h2>
+                        <p>
+                            I'm currently studying Software Engineering at UNSW
+                            and have experience working on projects in both
+                            frontend and backend frameworks. I've also written
+                            applications in <strong>C, Rust, Python and JavaScript.</strong>
+                        </p>
+                    </div>
+                    <img
+                        src={image}
+                        style={{
+                            width: "30%",
+                            marginTop: "6%",
+                            marginLeft: "6%",
+                        }}
+                    ></img>
+                </div>
             </div>
         </>
     );
