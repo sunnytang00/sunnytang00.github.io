b35f9229f42cbe7709465c292e824877437e547b
Sunny <derpynoobmaster@gmail.com>
2023-05-05 02:40:52 +1000

    added fading and border animation to project page

--- website/src/pages/projects.jsx
+++ website/src/pages/projects.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import "../styles/projectpage.css";
 const ProjectPage = () => {
     return (
         <>
-            <div style={{ color: "white" }}>Projects TODO</div>
+            <div style={{ color: "white" }}>
+                <div style={{ display: "flex" }}>
+                    <div className="nameContainer" style={{ display: "flex", flexDirection: "column", width: "30%", marginLeft: "7%", marginTop: "10%", fontSize: "40px" }}>
+                        <div className="projectName fromCenter">Project 1</div>
+                        <div className="projectName fromCenter">Project 2</div>
+                        <div className="projectName fromCenter">Project 3</div>
+                    </div>
+                    <div style={{width: "50%"}}>Project stuff</div>
+                </div>
+            </div>
         </>
     );
 };
