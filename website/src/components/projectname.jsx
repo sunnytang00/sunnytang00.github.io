783d5f1877705bebf2f5432fbce86ad98a505ec0
Sunny <derpynoobmaster@gmail.com>
2023-05-06 02:03:47 +1000

    added side animation for hover

---
+++ website/src/components/projectname.jsx
@@ -0,0 +1,15 @@
+import "../styles/projectname.css";
+import { Link } from "react-router-dom";
+const ProjectName = () => {
+    return (
+        <div className="projectLink projectName">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Project 1
+        </div>
+    );
+}
+
+export default ProjectName; 
--- website/src/pages/projects.jsx
+++ website/src/pages/projects.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "../styles/projectpage.css";
+import ProjectName from "../components/projectname";
 const ProjectPage = () => {
     return (
         <>
             <div style={{ color: "white" }}>
                 <div style={{ display: "flex" }}>
                     <div className="nameContainer" style={{ display: "flex", flexDirection: "column", width: "30%", marginLeft: "7%", marginTop: "10%", fontSize: "40px" }}>
-                        <div className="projectName fromCenter">Project 1</div>
+                        {/* <div className="projectName fromCenter">Project 1</div>
                         <div className="projectName fromCenter">Project 2</div>
-                        <div className="projectName fromCenter">Project 3</div>
+                        <div className="projectName fromCenter">Project 3</div> */}
+                        <ProjectName></ProjectName>
+                        <ProjectName></ProjectName>
+                        <ProjectName></ProjectName>
                     </div>
                     <div style={{width: "50%"}}>Project stuff</div>
                 </div>
